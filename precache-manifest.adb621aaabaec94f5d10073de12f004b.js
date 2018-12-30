96917579c87ef9ebb408803a45791dd348d10925
bjclark <bjclark@Remmert-MAC.local>
2018-12-30 16:48:45 -0500

    Updates

---
+++ precache-manifest.adb621aaabaec94f5d10073de12f004b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4da7d24854d73ed310e",
+    "url": "/playbook-maker/static/js/runtime~main.b4da7d24.js"
+  },
+  {
+    "revision": "32f48f293fd756679c37",
+    "url": "/playbook-maker/static/js/main.32f48f29.chunk.js"
+  },
+  {
+    "revision": "8743229f89cfb252efc7",
+    "url": "/playbook-maker/static/js/1.8743229f.chunk.js"
+  },
+  {
+    "revision": "32f48f293fd756679c37",
+    "url": "/playbook-maker/static/css/main.7aef60b2.chunk.css"
+  },
+  {
+    "revision": "cbb53cb98fc9449fae42688001faa297",
+    "url": "/playbook-maker/index.html"
+  }
+];
